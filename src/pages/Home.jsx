b67d8ce13e0395f4c8a658113931c8c7f2ab6895
Parshant Vardhan <parshantvardhan8@gmail.com>
2025-08-17 01:00:59 +0530

    fix video 5

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -104,9 +104,10 @@ const Home = () => {
             loop
             muted
             playsInline
+            preload="auto"
             className="w-full h-full object-cover"
           >
-            <source src="src\data\Projectcars.mp4" type="video/mp4" />
+            <source src="/Projectcars.mp4" type="video/mp4" />
             Your browser does not support the video tag.
           </video>
           <div className="absolute inset-0 bg-gradient-to-r from-black/80 to-transparent"></div>
